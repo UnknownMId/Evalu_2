83c3e7e43763deaa69a269c35c9309fc0190cc7d
unknown <fr.zambrano@duocuc.cl>
2022-05-15 16:24:59 -0400

    Modificación de Contacto

--- js/code.js
+++ js/code.js
@@ -8,7 +8,7 @@ fetch('https://apis.digital.gob.cl/dpa/regiones/')
     let options = data.map((item)=>{return `<option value="${item.nombre}">${item.nombre}</option>`})
     options = options.join('')
     //console.log(options)
-    let select = `<select onchange='setcomunas(this.value)' name="cars" id="cars" form="carform">${options}</select>`
+    let select = `<select onchange='setcomunas(this.value)' name="cars" id="cars" class="form-control" form="carform" >${options}</select>`
     console.log('cargada regiones')
     document.getElementById('formulario').innerHTML = select
     //console.log('regiones cargadas')
@@ -44,9 +44,29 @@ const setcomunas = (region)=>{
     let options = data.map((item)=>{return `<option value="${item.nombre}">${item.nombre}</option>`})
     options = options.join('')
     console.log(options)
-    let select = `<select name="cars" id="cars" form="carform">${options}</select>`
+    let select = `<select name="cars" id="cars" form="carform" class="form-control">${options}</select>`
     document.getElementById('comunas').innerHTML = select
 }
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
 
 
 
